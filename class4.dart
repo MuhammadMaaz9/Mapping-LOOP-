dabf1f2d7a0cff2a7f4fcb470c605093c83957de
MuhammadMaaz9 <100517667+MuhammadMaaz9@users.noreply.github.com>
2022-03-13 20:50:48 +0500

    Add files via upload

---
+++ class4.dart
@@ -0,0 +1,33 @@
+void main() {
+  //=========================MAPPing
+  // Map<dynamic,dynamic> data = {"Name": "Muhammad Maaz", "Number":"03172006091"};
+  // print(data);
+  // print(data.keys);
+  // print(data.values);
+
+  // Map data_1 = {
+  //   "Name": "Maaz",
+  //   "phone_num" : "03172006091",
+  //   "percentage" : "53.3",
+  //   "Address" : "Korangi no 1-1/2"
+  // };
+  // List my_list = [1,2,3,4,data_1];
+  // print("${data_1["phone_num"]}${data_1["Address"]}");
+  // print(my_list[4]["Name"]);
+// var words = {
+//   1:"Sky",
+//   2:"fly",
+//   3:"high",
+//   4:"fi"
+// };
+// words.remove(2);
+//  words.removeWhere((key, value) => value.startsWith('f'));
+//  print(words);
+// var fruit = "jaen";
+// var val = words.putIfAbsent(5, () => fruit);
+// print(val);
+//===============================================LOOPS
+for (var i = 0; i < 10; i) {
+  print("maaz coolboy");
+}
+}
